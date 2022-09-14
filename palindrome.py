e44545e02dea06f5986bf5a5dafa3150e110a00d
tejaswy2002 <113495301+tejaswy2002@users.noreply.github.com>
2022-09-14 17:13:36 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+def isPalindrome(s):
+	s1 = s.replace(' ', '')
+	s1 = s1.lower()
+	s2 = s1[::-1];
+	return True if s1 == s2 else False
+s = input("string is:")
+if (isPalindrome(s)):
+	print ("Sentence is palindrome.")
+else:
+	print ("Sentence is not palindrome.")
+OUTPUT:
+  string is:"race a car"
+Sentence is not palindrome.
+
+
+string is:"madam"
+Sentence is palindrome.
