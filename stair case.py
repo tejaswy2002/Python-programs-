0d68b4ad25f453d6d4c2d6bef91b3b6a9c65e83b
tejaswy2002 <113495301+tejaswy2002@users.noreply.github.com>
2022-09-14 18:15:55 +0530

    Create stair case.py

---
+++ stair case.py
@@ -0,0 +1,14 @@
+def fib(n):
+    if n <=1:
+        return n
+    return fib(n-1) +fib(n-2)
+def countways(s):
+    return fib (s+1)
+s = 4
+print("number of ways = ")
+print("count ways (s)")
+OUTPUT:
+number of ways = 
+count ways (s)
+2
+2
