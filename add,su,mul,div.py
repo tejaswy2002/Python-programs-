b80e64ccf7a6c30e54e99ebf2f767d58a35d99e9
tejaswy2002 <113495301+tejaswy2002@users.noreply.github.com>
2022-09-15 10:30:07 +0530

    Create add,su,mul,div.py

---
+++ add,su,mul,div.py
@@ -0,0 +1,12 @@
+#addition
+add=5+2
+print("addition: %d" %add)
+#subtraction
+sub=5-2
+print("subtraction: %d" %sub)
+#multiplication
+mul=5*2
+print("multiplication: %d" %mul)
+#division
+div=5/2
+print("division: %.2f" %div)
