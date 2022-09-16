9e392002df940a4b20a70d892ab9918bcf6e9ce0
tejaswy2002 <113495301+tejaswy2002@users.noreply.github.com>
2022-09-16 10:32:35 +0530

    Create palindrome or not.py

---
+++ palindrome or not.py
@@ -0,0 +1,13 @@
+string= input("Enter string:")
+if string == string[::-1]:
+ print(string, "is a palindrome")
+else:
+ print(string, "is not a palindrome")
+OUTPUT:
+  Enter string:madam
+madam is a palindrome
+
+
+Enter string:malayalam
+malayalam is a palindrome
+  
